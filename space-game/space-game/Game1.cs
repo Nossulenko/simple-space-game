42d4a8fcdb121b01ab6a9c450cc76ea5e7ccf53f
Nikolai Nossulenko <s089766@ap.be>
2018-08-08 01:08:33 +0200

    Added assets to project and a extra class for the ship

--- space-game/space-game/Game1.cs
+++ space-game/space-game/Game1.cs
@@ -12,18 +12,25 @@ namespace space_game
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+       
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.PreferredBackBufferWidth = 1280;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        Texture2D meteorite;
+        Texture2D spaceship;
+        Texture2D space;
+
+        SpriteFont spaceF;
+        SpriteFont timerF;
+
+        
+       
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -31,32 +38,27 @@ namespace space_game
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            meteorite = Content.Load<Texture2D>("asteroid");
+            space = Content.Load<Texture2D>("space");
+            spaceship = Content.Load<Texture2D>("ship");
+
+            spaceF = Content.Load<SpriteFont>("spaceFont");
+            timerF = Content.Load<SpriteFont>("timerFont");
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+       
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+      
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -67,15 +69,16 @@ namespace space_game
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+      
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
